f91a3ae19126bb8074052310694d3787ae62b2e2
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-04-30 14:16:44 +0200

    Adaptado para que se ejecute en varias maquinas

--- P3/Parte 2/Implementacion final/script_server.sh
+++ P3/Parte 2/Implementacion final/script_server.sh
@@ -1,14 +1,26 @@
 #!/bin/sh -e
 # ejecutar = Macro para compilacion y ejecucion del programa ejemplo
 # en una sola maquina Unix de nombre localhost.
-#echo
-#echo "Lanzando el ligador de RMI … "
+
 rmiregistry &
 
 echo
 echo "Compilando con javac ..."
 javac **/*.java
 
+if [[ "$#" -lt 1 ]]
+then
+    replicas=2
+    var=localhost
+elif [[ "$#" -eq 1 ]]
+then
+    replicas=$1
+    var=localhost
+else
+    replicas=$1
+    var=$2    
+fi
+
 echo
-echo "Lanzando el servidor"
-java -cp . -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=localhost -Djava.security.policy=server.policy Server/ServidorMain $1
+echo "Lanzando $replicas replicas con la direccion $var"
+java -cp . -Djava.rmi.server.codebase=file:./ -Djava.rmi.server.hostname=$var -Djava.security.policy=server.policy Server/ServidorMain $replicas
